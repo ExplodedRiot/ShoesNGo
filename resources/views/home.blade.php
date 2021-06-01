e06805a3d9954be894d522119d6d158a131762b3
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-01 16:02:45 +0700

    add

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,25 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-    <div class="col-md-12 mb-5">
-        <img src="{{ url('images/logo.png') }}" class="rounded mx-auto-d-block" width="700" alt="">
-    </div>
+        <div class="col-md-12 mb-5">
+            <img src="{{ url('images/logo.png') }}" class="rounded mx-auto d-block" width="700" alt="">
+        </div>
         @foreach($products as $product)
         <div class="col-md-4">
-        <div class="card">
-            <img src="{{ url('uploads') }}/{{ $product->image }}"  class="card-img-top" alt="..." >
-            <div class="card-body">
-                <h5 class="card-title">{{ $product->product-name }}</h5>
+            <div class="card">
+              <img src="{{ url('uploads') }}/{{ $product->image }}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">{{ $product->product_name }}</h5>
                 <p class="card-text">
-                <strong>Price :</strong> {{ number_format($product->price)}}K IDR <br>
-                <strong>Stock :</strong> {{ $product->stock }} <br>
-                <br>
-                <strong>Brand :</strong> {{ $product->description }}
+                    <strong>Price :</strong> Rp. {{ number_format($product->price)}} <br>
+                    <strong>Stock :</strong> {{ $product->stpck }} <br>
+                    <hr>
+                    <strong>Brand :</strong> <br>
+                    {{ $product->description }} 
                 </p>
-                <a href="#" class="btn btn-primary">Order Now</a>
-            </div>
-        </div>
+                <a href="{{ url('message') }}/{{ $product->id }}" class="btn btn-primary"><i class="fa fa-shopping-cart"></i> Message</a>
+              </div>
+            </div> 
         </div>
         @endforeach
     </div>
