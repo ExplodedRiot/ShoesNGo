2714c3d22eaee17d6ea90cc6d2cf1a9cd69dcaf2
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-01 14:41:20 +0700

    add

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -20,28 +20,28 @@ class ProductSeeder extends Seeder
                 "Price"=>'399.000',
                 "Stock"=>"25",
                 "Description"=>"Adidas",
-                "Image"=>"Adidas_lite_racer_2.0_(IDR 399k).jpg"
+                "Image"=>"images/Adidas_lite_racer_2.0_(IDR 399k).jpg"
             ],
             [
                 'product_name'=>'Adidas samba OG Classic',
                 "Price"=>'799.000',
                 "Stock"=>"25",
                 "Description"=>"Adidas",
-                "Image"=>"Adidas_samba_OG_Classic_(IDR 799k).jpg"
+                "Image"=>"images/Adidas_samba_OG_Classic_(IDR 799k).jpg"
             ],
             [
                 'product_name'=>'Adidas Stan Smith Polka White Leather',
                 "Price"=>'399.000',
                 "Stock"=>"25",
                 "Description"=>"Adidas",
-                "Image"=>"Adidas_stan_smith_polka_white_leather_(IDR 399k).jpg"
+                "Image"=>"images/Adidas_stan_smith_polka_white_leather_(IDR 399k).jpg"
             ],
             [
                 'product_name'=>'Adidas Terrex CMTK Outdoor',
                 "Price"=>'999.000',
                 "Stock"=>"25",
                 "Description"=>"Adidas",
-                "Image"=>"Adidas_Terrex_CMTK_Outdoor_(IDR 999k).jpg"
+                "Image"=>"images/Adidas_Terrex_CMTK_Outdoor_(IDR 999k).jpg"
             ]
         ]);
     }
