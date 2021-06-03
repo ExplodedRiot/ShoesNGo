8d2e6aa523b61bb96a86f45346b8d49187704705
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-04 00:01:50 +0700

    ed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,10 +52,10 @@
                         @else
                         <li class="nav-item">
                                 <?php
-                                 $main_order = \App\Models\Order::where('user_id', Auth::user()->id)->where('status',0)->first();
+                                 $main_order = \App\Models\Order::where('user_id', Auth::user()->id)->where('status',1)->first();
                                  if(!empty($main_order))
                                     {
-                                     $notif = \App\Models\OrderDetail::where('order_id', $main_order->id)->count(); 
+                                     $notif = \App\Models\OrderDetail::where('order_id', $main_order->id)->count();
                                     }
                                 ?>
                                 <a class="nav-link" href="{{ url('check-out') }}">
