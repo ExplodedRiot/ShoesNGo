181f27b4cc534532ca05b5d78b0721b8e0269d42
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-16 22:28:47 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,14 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('message/{id}',[App\Http\Controllers\MessageController::class, 'index'])->name('home');
 Route::post('message/{id}', [App\Http\Controllers\MessageController::class, 'message'])->name('message');
-Route::get('check-out', [App\Http\Controllers\MessageController::class, 'check-out'])->name('check-out');
+Route::get('check-out/{id}', [App\Http\Controllers\MessageController::class, 'check-out'])->name('check-out');
 Route::delete('check-out/{id}',[App\Http\Controllers\MessageController::class, 'delete'])->name('delete');
 
 Route::get('check-out-confirmation', [App\Http\Controllers\MessageController::class, 'confirm']);
 
 Route::get('profile',[App\Http\Controllers\ProfileController::class, 'index'])->name('home');
+Route::get('profile',[App\Http\Controllers\ProfileController::class, 'update'])->name('update');
+
+Route::get('history',[App\Http\Controllers\HistoryController::class, 'index'])->name('home');
+Route::get('history/{id}',[App\Http\Controllers\HistoryController::class, 'detail'])->name('detail');
 
