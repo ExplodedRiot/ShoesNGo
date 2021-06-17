51e9ba78c905ffd50fdc06eaba601ff655df5d34
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-18 02:52:16 +0700

    ed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,20 +2,44 @@
 
 namespace App\Http\Controllers;
 use Auth;
+use Alert;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-	public function __construnct()
-	{
-		$this->middleware('auth');
-	}
+     public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	public function index()
-	{
-		$user = User::where('id', Auth::user()->id)->first();
+    public function index()
+    {
+        $user = User::where('id', Auth::user()->id)->first();
 
-		return view('profile.index', compact('user'));
-	}
+        return view('profile.index', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+         $this->validate($request, [
+            'password'  => 'confirmed',
+        ]);
+
+        $user = User::where('id', Auth::user()->id)->first();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->address = $request->address;
+        $user->phone_number = $request->phone_number;
+        if(!empty($request->password))
+        {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->update();
+
+        Alert::success('User updated Successful ', 'Success');
+        return redirect('profile');
+    }
 }
+
