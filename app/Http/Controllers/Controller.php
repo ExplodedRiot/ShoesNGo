74919df1bcae350fe918b9f7ec9c31224f002379
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-17 17:47:51 +0700

    ed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 namespace Illuminate\Routing;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 use BadMethodCallException;
 
 abstract class Controller
