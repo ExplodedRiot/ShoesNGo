fb9810c2386280fc0a3d793cf30b28b2a05127c7
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-25 01:57:18 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,17 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('message/{id}',[App\Http\Controllers\MessageController::class, 'index'])->name('home');
-Route::post('message/{id}', [App\Http\Controllers\MessageController::class, 'message'])->name('message');
-Route::get('check-out', [App\Http\Controllers\MessageController::class, 'index'])->name('check-out');
-Route::delete('check-out',[App\Http\Controllers\MessageController::class, 'index'])->name('delete');
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+Route::get('message/{id}', 'App\Http\Controllers\MessageController@index');
+Route::post('message/{id}', 'App\Http\Controllers\MessageController@message');
+Route::get('check-out', 'App\Http\Controllers\MessageController@check_out');
+Route::delete('check-out/{id}','App\Http\Controllers\MessageController@delete');
 
-Route::get('check-out-confirmation', [App\Http\Controllers\MessageController::class, 'confirm']);
+Route::get('check-out-confirmation', 'App\Http\Controllers\MessageController@confirm');
 
-Route::get('profile',[App\Http\Controllers\ProfileController::class, 'index'])->name('home');
-Route::post('profile',[App\Http\Controllers\ProfileController::class, 'update'])->name('home');
+Route::get('profile','App\Http\Controllers\ProfileController@index');
+Route::post('profile','App\Http\Controllers\ProfileController@update');
 
-Route::get('history',[App\Http\Controllers\HistoryController::class, 'index'])->name('home');
-Route::get('history',[App\Http\Controllers\HistoryController::class, 'index'])->name('detail');
+Route::get('history','App\Http\Controllers\HistoryController@index');
+Route::get('history/{id}','App\Http\Controllers\HistoryController@detail');
 
