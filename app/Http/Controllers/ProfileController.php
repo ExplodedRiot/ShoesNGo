c310d1a44278990d67d946aeab382d263b09eac2
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-24 21:13:15 +0700

    ed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -48,8 +48,7 @@ class ProfileController extends Controller
         if($request->hasFile('avatar')){
             $avatar = $request->file('avatar');
             $filename = time() . '.' . $avatar->getClientOriginalExtension();
-            Image::make($avatar)->resize(300, 300)->save(public_path('images' . $filename));
-
+            Image::make($avatar)->resize(300, 300)->save(public_path('/images/avatar/' . $filename));
             $user = Auth::user();
             $user->avatar = $filename;
             $user->save();
