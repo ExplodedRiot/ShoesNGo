ce662609b6d3b8f999186e043ec50a286398b784
ExplodedRiot <alive_ananda@yahoo.com>
2021-06-25 07:40:12 +0700

    ed

--- resources/views/message/check_out.blade.php
+++ resources/views/message/check_out.blade.php
@@ -41,8 +41,8 @@
                                 </td>
                                 <td>{{ $order_detail->product->product_name }}</td>
                                 <td>{{ $order_detail->amount }} sneakers</td>
-                                <td align="right">{{ number_format($order_detail->product->Price) }}K IDR</td>
-                                <td align="right">{{ number_format($order_detail->total_price) }}K IDR</td>
+                                <td align="right">{{ number_format($order_detail->product->Price) }} IDR</td>
+                                <td align="right">{{ number_format($order_detail->total_price) }} IDR</td>
                                 <td>
                                     <form action="{{ url('check-out') }}/{{ $order_detail->id }}" method="post">
                                         @csrf
